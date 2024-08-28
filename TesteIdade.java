e5765bd431f94065c6bf88b65510f56dcf0ea210
eshy-y <143760987+eshy-y@users.noreply.github.com>
2024-08-28 12:34:51 +0000

    add: Código de testes

---
+++ TesteIdade.java
@@ -0,0 +1,24 @@
+class TesteIdade {
+    public static void main(String[] args) {
+        int idade;
+        idade = 21;
+
+        int idadeNoAnoQueVem = idade + 1;
+
+        double resultado = idade / 2;
+
+        
+        boolean maiorIdade;
+        maiorIdade = idade >= 18;
+
+        boolean tipoSangue;
+        tipoSangue = 'a' == 'A';
+
+        int i = 5;
+        int j = i;
+        i = i + 1;
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
